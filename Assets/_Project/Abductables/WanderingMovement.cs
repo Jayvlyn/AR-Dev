1fa763e612492ac43e7d36ec63746e5683d0fe4c
Jacob Leonardo <leonardojacob257@gmail.com>
2025-07-25 12:41:16 -0600

    Cow State

--- Assets/_Project/Abductables/WanderingMovement.cs
+++ Assets/_Project/Abductables/WanderingMovement.cs
@@ -5,48 +5,43 @@ using Random = UnityEngine.Random;
 
 public class WanderingMovement : MonoBehaviour
 {
+    public enum State
+    {
+        IDLE, WALKING, EATING, BEING_ABDUCTED, FALLING
+    }
+    public State currentState;
+
     private Vector3 targetPosition;
     [SerializeField] private float moveSpeed;
     [SerializeField] private float tolerance;
     [SerializeField, MinMaxSlider(0f, 30)] private Vector2 destinationChangeDelay;
     private float timeOfDestinationChange;
     private bool reachedDestination;
+    [SerializeField] Animator animator;
+    private float timeOfEat;
 
 
-    private bool inbeam;
     public void GoUpwards()
     {
-        inbeam = true;
+        ChangeState(State.BEING_ABDUCTED);
     }
     public void StopGoUpwards()
     {
-        inbeam = false;
+        ChangeState(State.FALLING);
     }
 
     private void Start()
     {
-        targetPosition = WanderableArea.activeWanderableArea.GetRandomPosition();
+        ChangeState(State.WALKING);
     }
 
     private void Update()
     {
-        if (inbeam)
-        {
-            transform.Translate(Vector3.up * Time.deltaTime);
-        }
-        else
-        {
-            AttemptApproachDestination();
-            CheckReachDestination();
-            AttemptChangeDestination();
-            
-        }
+        ProcessState();
     }
 
-    private void AttemptApproachDestination()
+    private void ApproachDestination()
     {
-        if(reachedDestination) return;
-
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
         RotateTowardsDestination();
     }
@@ -68,17 +63,102 @@ public class WanderingMovement : MonoBehaviour
     {
         if(reachedDestination) return;
         reachedDestination = (Vector3.Distance(transform.position, targetPosition) < tolerance);
-        if(reachedDestination) timeOfDestinationChange = Time.time + Random.Range(destinationChangeDelay.x, destinationChangeDelay.y);
+        if (reachedDestination)
+        {
+            timeOfDestinationChange = Time.time + Random.Range(destinationChangeDelay.x, destinationChangeDelay.y);
+            ChangeState(State.IDLE);
+        }
     }
 
     private void AttemptChangeDestination()
     {
-        if (!reachedDestination) return;
-
         if (Time.time > timeOfDestinationChange)
         {
-            targetPosition = WanderableArea.activeWanderableArea.GetRandomPosition();
-            reachedDestination = false;
+            ChangeState(State.WALKING);
+        }
+        else
+        {
+            if(currentState == State.IDLE)
+            {
+                if(Random.Range(0,1000) == 0)
+                {
+                    ChangeState(State.EATING);
+                }
+            }
         }
     }
+
+    private void ChangeState(State newState)
+    {
+		switch (currentState) // exit
+		{
+			case State.IDLE:
+				break;
+			case State.WALKING:
+				SetTrigger("StopWalk");
+				break;
+			case State.EATING:
+				break;
+			case State.BEING_ABDUCTED:
+				break;
+			case State.FALLING:
+				break;
+		}
+
+		currentState = newState;
+
+		switch (newState)
+		{
+			case State.IDLE:
+				break;
+			case State.WALKING:
+				targetPosition = WanderableArea.activeWanderableArea.GetRandomPosition();
+				reachedDestination = false;
+				SetTrigger("EnterWalk");
+				break;
+			case State.EATING:
+				SetTrigger("Eat");
+                timeOfEat = Time.time;
+				break;
+			case State.BEING_ABDUCTED:
+				break;
+			case State.FALLING:
+				break;
+		}
+	}
+
+    private void ProcessState()
+    {
+		switch (currentState)
+		{
+			case State.IDLE:
+				AttemptChangeDestination();
+				break;
+			case State.WALKING:
+				ApproachDestination();
+				CheckReachDestination();
+				break;
+			case State.EATING:
+                if(Time.time > timeOfEat + 3)
+                {
+                    ChangeState(State.IDLE);
+                }
+				break;
+			case State.BEING_ABDUCTED:
+				transform.Translate(Vector3.up * Time.deltaTime);
+				break;
+			case State.FALLING:
+				break;
+		}
+	}
+
+	string currentTrigger;
+	void SetTrigger(string triggerName)
+	{
+		if (!string.IsNullOrEmpty(currentTrigger))
+			animator.ResetTrigger(currentTrigger); // Clear the previous trigger
+
+		animator.SetTrigger(triggerName); // Set the new trigger
+		currentTrigger = triggerName; // Remember the new one
+	}
 }
