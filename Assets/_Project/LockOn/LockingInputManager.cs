9a5d73e7fa48fdf27f23a65c904931ba8076659b
Chase Guymon <chase.guymon@q.com>
2025-07-27 13:45:30 -0600

    Add a touch screen input mech

--- Assets/_Project/LockOn/LockingInputManager.cs
+++ Assets/_Project/LockOn/LockingInputManager.cs
@@ -1,12 +1,13 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 public class LockingInputManager : MonoBehaviour
 {
     [SerializeField] private LockOnController lockControl;
     private void Update()
     {
-        if (Mouse.current.leftButton.wasPressedThisFrame)
+        if (Mouse.current.leftButton.wasPressedThisFrame || Touch.activeTouches.Count > 0)
         {
             lockControl.TryLockOnTarget();
         }
