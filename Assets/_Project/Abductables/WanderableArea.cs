42e8df26bd70137da7ce3ec5dc585ffd3b52eebc
Chase Guymon <chase.guymon@q.com>
2025-07-28 19:21:46 -0600

    circle

--- Assets/_Project/Abductables/WanderableArea.cs
+++ Assets/_Project/Abductables/WanderableArea.cs
@@ -30,17 +30,37 @@ public class WanderableArea : MonoBehaviour
             }
         }
     }
+
+    private int maxLoops = 50;
     public Vector3 GetRandomPosition(int planeIndex)
     {
         Collider area = colliders[planeIndex];
         Bounds bounds = area.bounds;
 
-        Vector3 randomPoint = new Vector3(
-            Random.Range(bounds.min.x, bounds.max.x),
-            Random.Range(bounds.min.y, bounds.max.y),
-            Random.Range(bounds.min.z, bounds.max.z)
-        );
+        int loopCount = 0;
+        Vector3 randomPoint;
+        do
+        {
+            loopCount++;
+            randomPoint = new Vector3(
+                Random.Range(bounds.min.x, bounds.max.x),
+                Random.Range(bounds.min.y, bounds.max.y),
+                Random.Range(bounds.min.z, bounds.max.z)
+            );
+        } while (!(IsValidPoint(randomPoint) || loopCount >= maxLoops));
 
         return area.ClosestPoint(randomPoint);
+    }
+
+    [SerializeField] private float distanceFromPlayer = 0.5f;
+    private bool IsValidPoint(Vector3 point)
+    {
+        point.y = 0;
+        return point.magnitude > distanceFromPlayer;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(Vector3.up, distanceFromPlayer);
     }
 }
